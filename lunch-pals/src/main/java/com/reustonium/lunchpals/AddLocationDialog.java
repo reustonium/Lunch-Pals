f718464fec07bcbb3d17eb682404a23dd1a16a55
reustonium <andrew.ruestow@gmail.com>
2014-01-02 22:39:12 -0500

    dialog fragment is still broken.

--- lunch-pals/src/main/java/com/reustonium/lunchpals/AddLocationDialog.java
+++ lunch-pals/src/main/java/com/reustonium/lunchpals/AddLocationDialog.java
@@ -3,16 +3,49 @@ package com.reustonium.lunchpals;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 /**
  * Created by Andrew on 1/1/14.
  */
 public class AddLocationDialog extends DialogFragment {
 
+    EditText locationName = new EditText(getActivity().getApplicationContext());
+
     public Dialog onCreateDialog(Bundle savedInstanceState){
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("Add New Location");
+        builder.setTitle("Add New Location");
+        builder.setView(locationName);
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                ParseObject location = new ParseObject("Location");
+                location.put("name", locationName.getText());
+
+                location.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e==null){
+                            //TODO Close Alert
+                        }
+                    }
+                });
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
         return builder.create();
     }
 }
