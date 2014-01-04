0262fb9a488502d5cb3575b5b261a37105f9459e
reustonium <andrew.ruestow@gmail.com>
2014-01-03 22:08:27 -0500

    working on broken alert dialog, think I need to use inner classes

--- lunch-pals/src/main/java/com/reustonium/lunchpals/AddLocationDialog.java
+++ lunch-pals/src/main/java/com/reustonium/lunchpals/AddLocationDialog.java
@@ -16,35 +16,9 @@ import com.parse.SaveCallback;
  */
 public class AddLocationDialog extends DialogFragment {
 
-    EditText locationName = new EditText(getActivity().getApplicationContext());
-
     public Dialog onCreateDialog(Bundle savedInstanceState){
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder = new AlertDialog.Builder(LocationsFragment);
         builder.setTitle("Add New Location");
-        builder.setView(locationName);
-        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                ParseObject location = new ParseObject("Location");
-                location.put("name", locationName.getText());
-
-                location.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if (e==null){
-                            //TODO Close Alert
-                        }
-                    }
-                });
-            }
-        });
-
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-            }
-        });
 
         return builder.create();
     }
