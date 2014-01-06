6bc554cfc42f3adc7613dd253ef7c85af57b55d5
reustonium <andrew.ruestow@gmail.com>
2014-01-05 19:39:10 -0500

    working on a simplified version to track only who is in for lunch

--- lunch-pals/src/main/java/com/reustonium/lunchpals/HomeActivity.java
+++ lunch-pals/src/main/java/com/reustonium/lunchpals/HomeActivity.java
@@ -1,148 +1,99 @@
 package com.reustonium.lunchpals;
 
-import java.util.Locale;
-
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
-import com.parse.ParseAnalytics;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
-public class HomeActivity extends ActionBarActivity implements ActionBar.TabListener,StatusFragment.OnFragmentInteractionListener {
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    ViewPager mViewPager;
+public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_home);
-        ParseAnalytics.trackAppOpened(getIntent());
 
-        // Set up the action bar.
-        final ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        // When swiping between different sections, select the corresponding
-        // tab. We can also use ActionBar.Tab#select() to do this if we have
-        // a reference to the Tab.
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                actionBar.setSelectedNavigationItem(position);
-            }
-        });
-
-        // For each of the sections in the app, add a tab to the action bar.
-        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            // Create a tab with text corresponding to the page title defined by
-            // the adapter. Also specify this Activity object, which implements
-            // the TabListener interface, as the callback (listener) for when
-            // this tab is selected.
-            actionBar.addTab(
-                    actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getPageTitle(i))
-                            .setTabListener(this));
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
         }
     }
 
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in
-        // the ViewPager.
-        mViewPager.setCurrentItem(tab.getPosition());
-    }
+    public static class PlaceholderFragment extends Fragment {
 
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
+        boolean hazPangs;
 
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
+        public PlaceholderFragment() {
         }
 
         @Override
-        public Fragment getItem(int position) {
-            switch(position){
-                case 0:
-                    return new StatusFragment();
-                case 1:
-                    return new LocationsFragment();
-                case 2:
-                    return new StatusFragment();
-            }
-            return new StatusFragment();
-        }
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
+            // Set Greeting
+            ParseUser user = ParseUser.getCurrentUser();
 
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
-                case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
-            }
-            return null;
+            TextView textView = (TextView) rootView.findViewById(R.id.textView_greeting);
+            String greeting = String.format("Hi Pal! Do you haz pangs today %s?", user.getUsername());
+            textView.setText(greeting);
+
+            // Setup Switch - Pull current status if already set
+            ParseQuery<ParseObject> query = ParseQuery.getQuery("Status");
+            query.whereEqualTo("createdBy", user);
+            query.orderByAscending("createdAt");
+
+            query.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> parseObjects, ParseException e) {
+                    if(e==null){
+                        for(ParseObject obj : parseObjects){
+                            Log.v("!!!", String.format("Craeted at: %d with status: %b", obj.getDate("createdAt"), obj.getBoolean("hazPangs")));
+
+                        }
+
+                        Log.v("!!!", String.format("there are: %d objects", parseObjects.size()));
+                    } else {
+                        Log.v("!!!", e.getMessage());
+                    }
+                }
+            });
+
+            // Setup Switch to default to false
+
+            // Handle Switch OnClick
+            Switch mSwitch = (Switch) rootView.findViewById(R.id.switch_hazPangs);
+            mSwitch.setOnCheckedChangeListener( new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    ParseObject status = new ParseObject("Status");
+                    status.put("hazPangs", b);
+                    status.put("createdBy", ParseUser.getCurrentUser());
+                    status.saveInBackground();
+                }
+            });
+
+            // Setup Listview
+
+            return rootView;
         }
     }
+
 }
