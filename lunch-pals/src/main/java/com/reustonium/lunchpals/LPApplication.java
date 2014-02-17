063c6dd768a77847d13d1a190041b320d2a11e3d
reustonium <andrew.ruestow@gmail.com>
2014-02-16 23:15:43 -0500

    removed Status class

--- lunch-pals/src/main/java/com/reustonium/lunchpals/LPApplication.java
+++ lunch-pals/src/main/java/com/reustonium/lunchpals/LPApplication.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseObject;
 import com.reustonium.lunchpals.models.Nudge;
-import com.reustonium.lunchpals.models.Status;
 
 /**
  * Created by Andrew on 2/2/14.
@@ -16,7 +15,6 @@ public class LPApplication extends Application {
     public void onCreate() {
         super.onCreate();
         ParseObject.registerSubclass(Nudge.class);
-        ParseObject.registerSubclass(Status.class);
         Parse.initialize(this, AppConsts.APPID, AppConsts.CLIENTKEY);
     }
 }
