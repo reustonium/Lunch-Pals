1131cf4e9d8f11946cbe4ad05a743d46c33eb317
reustonium <andrew.ruestow@gmail.com>
2016-01-18 11:33:33 -0500

    Add travis config file

--- app/src/main/java/com/reustonium/lunchpals/MainActivity.java
+++ app/src/main/java/com/reustonium/lunchpals/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Hi Pal!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
