853152c5c955fa864f2bc5f00a4923a8c0ebd182
reustonium <andrew.ruestow@gmail.com>
2016-01-30 14:43:09 -0500

    Add Pals Package

---
+++ app/src/main/java/com/reustonium/lunchpals/Pals/PalsActivity.java
@@ -0,0 +1,16 @@
+package com.reustonium.lunchpals.Pals;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.reustonium.lunchpals.R;
+
+public class PalsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pal_list);
+
+    }
+}
---
+++ app/src/main/java/com/reustonium/lunchpals/Pals/PalsFragment.java
@@ -0,0 +1,7 @@
+package com.reustonium.lunchpals.Pals;
+
+/**
+ * Created by andrew on 1/30/16.
+ */
+public class PalsFragment {
+}
