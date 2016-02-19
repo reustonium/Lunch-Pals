67277cd6aec310e3c08fea3a4881209cf7ce62aa
reustonium <andrew.ruestow@gmail.com>
2016-02-19 13:25:03 -0500

    Add Toolbar Test

--- app/src/androidTest/java/com/reustonium/lunchpals/MainActivityTest.java
+++ app/src/androidTest/java/com/reustonium/lunchpals/MainActivityTest.java
@@ -2,16 +2,22 @@ package com.reustonium.lunchpals;
 
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import rx.Observable;
@@ -22,9 +28,13 @@ import com.reustonium.lunchpals.ui.main.MainActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
 import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4.class)
@@ -71,50 +81,45 @@ public class MainActivityTest {
         }
     }
 
+
     @Test
-    public void listOfRibotsShowsDUP() {
-        List<Ribot> testDataRibots = TestDataFactory.makeListRibots(20);
+    public void toolbarShow() {
+        List<Ribot> testDataRibots = new ArrayList<>();
         when(component.getMockDataManager().getRibots())
                 .thenReturn(Observable.just(testDataRibots));
 
         main.launchActivity(null);
 
-        int position = 0;
-        for (Ribot ribot : testDataRibots) {
-            onView(withId(R.id.recycler_view))
-                    .perform(RecyclerViewActions.scrollToPosition(position));
-            String name = String.format("%s %s", ribot.profile.name.first,
-                    ribot.profile.name.last);
-            onView(withText(name))
-                    .check(matches(isDisplayed()));
-            onView(withText(ribot.profile.email))
-                    .check(matches(isDisplayed()));
-            position++;
-        }
+        CharSequence title = "Lunch Pals";
+        matchToolbarTitle(title);
     }
 
+    @Test
+    public void emptyRibotList() {
+        List<Ribot> testDataRibots = new ArrayList<>();
+        when(component.getMockDataManager().getRibots())
+                .thenReturn(Observable.just(testDataRibots));
 
-//    @Test
-//    public void toolbarShow() {
-//        List<Ribot> testDataRibots = new ArrayList<>();
-//        when(component.getMockDataManager().getRibots())
-//                .thenReturn(Observable.just(testDataRibots));
-//
-//        main.launchActivity(null);
-//        onView(withId(R.id.toolbar))
-//                .check(matches(withText("Lunch Pals")));
-//    }
-//
-//    @Test
-//    public void emptyRibotList() {
-//        List<Ribot> testDataRibots = new ArrayList<>();
-//        when(component.getMockDataManager().getRibots())
-//                .thenReturn(Observable.just(testDataRibots));
-//
-//        main.launchActivity(null);
-//
-//        onView(withText(R.string.empty_ribots)).inRoot(withDecorView(not(main.getActivity()
-//                .getWindow().getDecorView()))).check(matches(isDisplayed()));
-//    }
+        main.launchActivity(null);
 
+        onView(withText(R.string.empty_ribots)).inRoot(withDecorView(not(main.getActivity()
+                .getWindow().getDecorView()))).check(matches(isDisplayed()));
+    }
+
+    private static ViewInteraction matchToolbarTitle(CharSequence title) {
+        return onView(isAssignableFrom(Toolbar.class))
+                .check(matches(withToolbarTitle(is(title))));
+    }
+
+    private static Matcher<Object> withToolbarTitle(final Matcher<CharSequence> textMatcher) {
+        return new BoundedMatcher<Object, Toolbar>(Toolbar.class) {
+            @Override public boolean matchesSafely(Toolbar toolbar) {
+                return textMatcher.matches(toolbar.getTitle());
+            }
+            @Override public void describeTo(Description description) {
+                description.appendText("with toolbar title: ");
+                textMatcher.describeTo(description);
+            }
+        };
+    }
 }
