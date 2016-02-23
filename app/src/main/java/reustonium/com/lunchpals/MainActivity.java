342def60e71325f8e4672111f4c6267930247dfe
bardoyas <bardoyas.mccallister@gmail.com>
2016-02-23 16:21:26 -0500

    Add RecyclerView

--- app/src/main/java/reustonium/com/lunchpals/MainActivity.java
+++ app/src/main/java/reustonium/com/lunchpals/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
