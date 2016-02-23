13b4570752068ddd66afdf59bad6dc14ba4d4cce
bardoyas <bardoyas.mccallister@gmail.com>
2016-02-23 15:43:27 -0500

    Add toolbar

--- app/src/main/java/reustonium/com/lunchpals/MainActivity.java
+++ app/src/main/java/reustonium/com/lunchpals/MainActivity.java
@@ -1,7 +1,9 @@
 package reustonium.com.lunchpals;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Add Toolbar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.colorText));
+        setSupportActionBar(toolbar);
     }
 }
