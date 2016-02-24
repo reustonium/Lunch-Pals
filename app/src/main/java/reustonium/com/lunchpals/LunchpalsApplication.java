6ad73c742cb8db6c95946e547a896a536e36206a
reustonium <andrew.ruestow@gmail.com>
2016-02-23 20:42:44 -0500

    Add Application class

--- app/src/main/java/reustonium/com/lunchpals/LunchpalsApplication.java
+++ app/src/main/java/reustonium/com/lunchpals/LunchpalsApplication.java
@@ -1,7 +1,24 @@
 package reustonium.com.lunchpals;
 
 import android.app.Application;
+import android.content.Context;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 public class LunchpalsApplication extends Application{
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if(BuildConfig.DEBUG){
+            Fabric.with(this, new Crashlytics());
+        }
+    }
+
+    public static LunchpalsApplication get(Context context){
+        return (LunchpalsApplication) context.getApplicationContext();
+    }
 }
