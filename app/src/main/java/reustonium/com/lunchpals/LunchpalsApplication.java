73cfefbf0d7b4e40f4b33fca9bef94a56bb940be
reustonium <andrew.ruestow@gmail.com>
2016-02-28 14:29:32 -0500

    Remove Fabric

--- app/src/main/java/reustonium/com/lunchpals/LunchpalsApplication.java
+++ app/src/main/java/reustonium/com/lunchpals/LunchpalsApplication.java
@@ -3,9 +3,6 @@ package reustonium.com.lunchpals;
 import android.app.Application;
 import android.content.Context;
 
-import com.crashlytics.android.Crashlytics;
-
-import io.fabric.sdk.android.Fabric;
 import reustonium.com.lunchpals.injection.component.ApplicationComponent;
 import reustonium.com.lunchpals.injection.component.DaggerApplicationComponent;
 import reustonium.com.lunchpals.injection.module.ApplicationModule;
@@ -17,12 +14,6 @@ public class LunchpalsApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        final Fabric fabric = new Fabric.Builder(this)
-                .kits(new Crashlytics())
-                .debuggable(BuildConfig.DEBUG)
-                .build();
-        Fabric.with(fabric);
     }
 
     public static LunchpalsApplication get(Context context) {
