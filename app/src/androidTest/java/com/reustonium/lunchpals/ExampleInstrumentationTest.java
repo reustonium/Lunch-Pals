7c9dba1baf72f8d92ac7a6be9142d863bde6fccd
bardoyas <bardoyas.mccallister@gmail.com>
2016-06-27 14:29:51 -0400

    Change CircleCI settings

--- app/src/androidTest/java/com/reustonium/lunchpals/ExampleInstrumentationTest.java
+++ app/src/androidTest/java/com/reustonium/lunchpals/ExampleInstrumentationTest.java
@@ -2,7 +2,6 @@ package com.reustonium.lunchpals;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.filters.MediumTest;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
@@ -16,7 +15,6 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@MediumTest
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentationTest {
     @Test
