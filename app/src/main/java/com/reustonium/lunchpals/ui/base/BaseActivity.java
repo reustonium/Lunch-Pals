10d77e248cc94ba6a035f4286cc3f02a5118c396
bardoyas <bardoyas.mccallister@gmail.com>
2016-06-27 16:30:56 -0400

    Add Login

--- app/src/main/java/com/reustonium/lunchpals/ui/base/BaseActivity.java
+++ app/src/main/java/com/reustonium/lunchpals/ui/base/BaseActivity.java
@@ -1,12 +1,8 @@
 package com.reustonium.lunchpals.ui.base;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.reustonium.lunchpals.LunchPalsApplication;
 import com.reustonium.lunchpals.injection.component.ActivityComponent;
 import com.reustonium.lunchpals.injection.component.DaggerActivityComponent;
@@ -16,28 +12,11 @@ import com.reustonium.lunchpals.ui.signup.SignUpActivity;
 public class BaseActivity extends AppCompatActivity {
 
     private ActivityComponent mActivityComponent;
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
     private static final String TAG = SignUpActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    Log.d(TAG, "onAuthStateChanged:signed_out:");
-                }
-            }
-        };
-
-        mAuth.addAuthStateListener(mAuthListener);
     }
 
     public ActivityComponent getActivityComponent() {
--- app/src/main/java/com/reustonium/lunchpals/ui/signup/SignUpActivity.java
+++ app/src/main/java/com/reustonium/lunchpals/ui/signup/SignUpActivity.java
@@ -1,20 +1,105 @@
 package com.reustonium.lunchpals.ui.signup;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.reustonium.lunchpals.R;
 import com.reustonium.lunchpals.ui.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import javax.inject.Inject;
 
 public class SignUpActivity extends BaseActivity {
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private static final String TAG = SignUpActivity.class.getName();
     @Inject SignUpPresenter mSignUpPresenter;
 
-    
+    @BindView(R.id.password) EditText mPasswordView;
+    @BindView(R.id.email) EditText mEmailView;
+    @BindView(R.id.sign_in_btn) Button mSignInButton;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActivityComponent().inject(this);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out:");
+                }
+            }
+        };
+        mAuth.addAuthStateListener(mAuthListener);
+
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == 769 || actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_NULL) {
+                    createUser();
+                    // don't consume the event, so the keyboard can also be hidden
+                    // http://stackoverflow.com/questions/2342620/how-to-hide-keyboard-after-typing-in-edittext-in-android#comment20849208_10184099
+                    return false;
+                }
+                return false;
+            }
+        });
+
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createUser();
+            }
+        });
+    }
+
+    private void createUser() {
+
+        //TODO Validate Inputs
+
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
