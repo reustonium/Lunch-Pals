ebb1df60ebadfbbb141eb1530145f8826e60cfa4
bardoyas <bardoyas.mccallister@gmail.com>
2016-06-27 16:01:57 -0400

    Redesign SignUp UI

--- app/src/main/java/com/reustonium/lunchpals/ui/base/BaseActivity.java
+++ app/src/main/java/com/reustonium/lunchpals/ui/base/BaseActivity.java
@@ -1,20 +1,43 @@
 package com.reustonium.lunchpals.ui.base;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.reustonium.lunchpals.LunchPalsApplication;
 import com.reustonium.lunchpals.injection.component.ActivityComponent;
 import com.reustonium.lunchpals.injection.component.DaggerActivityComponent;
 import com.reustonium.lunchpals.injection.module.ActivityModule;
+import com.reustonium.lunchpals.ui.signup.SignUpActivity;
 
 public class BaseActivity extends AppCompatActivity {
 
     private ActivityComponent mActivityComponent;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = SignUpActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out:");
+                }
+            }
+        };
+
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     public ActivityComponent getActivityComponent() {
@@ -26,5 +49,4 @@ public class BaseActivity extends AppCompatActivity {
         }
         return mActivityComponent;
     }
-
 }
--- app/src/main/java/com/reustonium/lunchpals/ui/signup/SignUpActivity.java
+++ app/src/main/java/com/reustonium/lunchpals/ui/signup/SignUpActivity.java
@@ -1,11 +1,6 @@
 package com.reustonium.lunchpals.ui.signup;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.util.Log;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.reustonium.lunchpals.R;
 import com.reustonium.lunchpals.ui.base.BaseActivity;
 
@@ -13,30 +8,13 @@ import javax.inject.Inject;
 
 public class SignUpActivity extends BaseActivity {
 
-    private static final String TAG = SignUpActivity.class.getName();
     @Inject SignUpPresenter mSignUpPresenter;
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActivityComponent().inject(this);
         setContentView(R.layout.activity_sign_up);
-
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    Log.d(TAG, "onAuthStateChanged:signed_out:");
-                }
-            }
-        };
-
-        mAuth.addAuthStateListener(mAuthListener);
-
     }
 }
